479f7324d408bb1e86fd288533d16583a0454bdf
Josh Knopp <github@joshknopp.com>
2024-04-27 13:30:17 -0400

    chore: wip main is now runnable

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 from flask import Flask, request, jsonify
+import main
 
 app = Flask(__name__)
 
 @app.route('/webhook', methods=['POST'])
 def webhook():
     notification = request.json
-    # Process the notification here
-    print(notification)
+    main.process_notification(notification)
     return jsonify({'success': True})
 
 if __name__ == '__main__':
